e8f02f6dd4e9c5e72d289d4a879262cc00bfae87
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-12-14 12:45:11 +0900

    chore: fpsを10で固定してる理由を追記

--- image_recognition.py
+++ image_recognition.py
@@ -48,6 +48,7 @@ class ImageRecognition(customtkinter.CTkFrame):
             # 保存用の動画ファイル設定
             w = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
             h = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            # 大体決め打ちで10fps, 石垣島PCで推論する時のfpsと合わせる
             fps = 10.
             fourcc = cv2.VideoWriter_fourcc(*"mp4v")
             now = datetime.now().strftime("%Y%m%d_%H%M%S")
