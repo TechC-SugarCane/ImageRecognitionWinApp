b3f1611e3756f4c8e830b06508cf45fe30475779
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-12-24 21:54:29 +0900

    feat: モデル出力のラベルを別のファイルで管理する

--- function/const/crop.py
+++ function/const/crop.py
@@ -4,3 +4,5 @@ type CropType = Literal["sugarcane", "pineapple"]
 type LabelName = Literal["sugarcane", "pineapple", "weed"]
 
 CROP_NAME_LIST: list[CropType] = ["sugarcane", "pineapple"]
+SUGARCANE_LABEL_LIST: list[LabelName] = ["sugarcane", "weed"]
+PINEAPPLE_LABEL_LIST: list[LabelName] = ["pineapple", "weed"]
