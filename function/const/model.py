1957d4462dd09c56b766b0f4d82e087a128e33b0
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2025-02-08 20:48:10 +0900

    feat: YOLOv7に関連する処理を削除

--- function/const/model.py
+++ function/const/model.py
@@ -1,5 +1,5 @@
 from typing import Literal
 
-type ModelType = Literal["YOLOv7", "YOLOv9", "YOLOv10"]
+type ModelType = Literal["YOLOv9", "YOLOv10"]
 
-MODEL_NAME_LIST: list[ModelType] = ["YOLOv7", "YOLOv9", "YOLOv10"]
+MODEL_NAME_LIST: list[ModelType] = ["YOLOv9", "YOLOv10"]
